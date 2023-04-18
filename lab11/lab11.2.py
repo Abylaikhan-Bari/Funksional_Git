ad156565570592cf34723b1c865f1b768cddb2f4
Abylaikhan-Bari <81789869+Abylaikhan-Bari@users.noreply.github.com>
2023-04-18 09:52:55 +0600

    on_bir

--- lab11/lab11.2.py
+++ lab11/lab11.2.py
@@ -6,12 +6,13 @@ canvas = tk.Canvas(root, width=800, height=500)
 canvas.pack()
 
 # Нарисуем буквы имени Abylaikhan
-canvas.create_text(100, 100, text="A", font=("Arial", 72))
-canvas.create_text(200, 100, text="b", font=("Arial", 72))
-canvas.create_text(300, 100, text="y", font=("Arial", 72))
-canvas.create_text(400, 100, text="l", font=("Arial", 72))
-canvas.create_text(500, 100, text="a", font=("Arial", 72))
-canvas.create_text(600, 100, text="i", font=("Arial", 72))
+canvas.create_text(100, 100, text="S", font=("Arial", 72))
+canvas.create_text(200, 100, text="t", font=("Arial", 72))
+canvas.create_text(300, 100, text="u", font=("Arial", 72))
+canvas.create_text(400, 100, text="d", font=("Arial", 72))
+canvas.create_text(500, 100, text="e", font=("Arial", 72))
+canvas.create_text(600, 100, text="n", font=("Arial", 72))
+canvas.create_text(700, 100, text="t", font=("Arial", 72))
 
 # Запускаем основной цикл окна
 root.mainloop()
